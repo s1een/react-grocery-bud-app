76031cc13dde681b3ebf51a625a0875e2fbfb7f1
r3ason_why <windeks2017@gmail.com>
2022-08-08 14:54:35 +0300

    React Grocery Bud

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState(
+    localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : []
+  );
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name) {
+      showAlert(true, "danger", "please enter value");
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "info", "value changed");
+    } else {
+      showAlert(true, "success", "Item Added to the list");
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  }
+
+  function clearList() {
+    showAlert(true, "danger", "empty list");
+    setList([]);
+  }
+
+  function showAlert(show = false, type = "", msg = "") {
+    setAlert({ show, type, msg });
+  }
+
+  function removeItem(id) {
+    showAlert(true, "danger", "item removed");
+    setList(list.filter((item) => item.id !== id));
+  }
+
+  function editItem(id) {
+    const myItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(myItem.title);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button onClick={clearList} className="clear-btn">
+            clear Items
+          </button>
+        </div>
+      )}
+    </section>
   );
 }
 
